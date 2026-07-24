08d930ddf7202d8a20812cc5f6a5ff753ae6788e
jmbe <jmbe@itu.dk>
2026-07-24 14:00:22 +0200

    Began abstracting the Vertex and Index buffers

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,25 +6,10 @@
 #include<string>
 #include<sstream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-   x;\
-   ASSERT(GLLogCall(#x, __FILE__,__LINE__))
+#include"Rendere.h"
 
-static void GLClearError() 
-{
-    while (glGetError == GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line) 
-{
-    while (GLenum error = glGetError())
-    {
-        std::cout << "[OpenGL_Error] (" << error << ")" << function << " " << file << " Line: " << line << std::endl;
-        return false;
-    }
-    return true;
-}
+#include"VertexBuffer.h"
+#include"IndexBuffer.h"
 
 struct ShaderProgramSource 
 {
@@ -121,7 +106,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -153,24 +138,20 @@ int main(void)
     };
 
     unsigned int vao;
-    GLCall(glGenVertexArrays(1, &vao)); 
+    GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
 
-    unsigned int buffer;
-    GLCall(glGenBuffers(1,&buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float),positions,GL_STATIC_DRAW));
+
+
+    VertexBuffer vb(positions, 4*2*sizeof(float));
 
     GLCall(glEnableVertexAttribArray(0));
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
-    unsigned int ibo;
-    GLCall(glGenBuffers(1, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indeces, GL_STATIC_DRAW));
+    IndexBuffer ib(indeces, 6);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    unsigned int shader = CreateShader(source.VertexSource,source.FragmentSource);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
 
@@ -181,7 +162,7 @@ int main(void)
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
     GLCall(glUseProgram(0));
-    
+
     float r = 0.0f;
 
     float increment = 0.05f;
@@ -196,17 +177,17 @@ int main(void)
         GLCall(glUniform4f(location, r, 0.0f, 1.0f, 1.0f));
 
         GLCall(glBindVertexArray(vao));
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-        
+        ib.Bind();
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f)
             increment = -0.05f;
         else if (r < 0.0f)
             increment = 0.05f;
-        
+
         r += increment;
-       
+
         /* Swap front and back buffers */
         GLCall(glfwSwapBuffers(window));
 
